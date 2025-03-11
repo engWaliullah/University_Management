972842db51b909506155fbf7a55396919041319d
MD Waliullah <waliullah9099@gmail.com>
2025-03-11 10:17:45 +0600

    create custom handlar for zod error to organized or handled all error in one way fllow

--- src/app/middlewares/globalErrorHanalar.ts
+++ src/app/middlewares/globalErrorHanalar.ts
@@ -1,19 +1,39 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
+import handleZodError from '../errors/handleZodError';
 
-const globalErrorHanalar = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+const globalErrorHanalar : ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
 ) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Something went wrong';
   error: err;
 
+  let errorSources : TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    }
+  ]
+
+  // check if the error is a zod error
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err.stack : undefined,
   });
 };
 
