63e34112be1de6212346aa5f5c4db069af230ba2
MD Waliullah <waliullah9099@gmail.com>
2025-04-16 07:30:51 +0600

    set meta data when get all data

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,156 +1,193 @@
 import httpStatus from 'http-status';
-import { Course, CourseFaculty } from "./course.model"
-import { TCourse, TCoursefaculty } from "./course.interface"
-import QuireBuilder from "../../builder/QueryBuilder";
-import { CourseSearchableFields } from "./course.constant";
-import mongoose from "mongoose";
-import AppError from "../../errors/AppError";
+import { Course, CourseFaculty } from './course.model';
+import { TCourse, TCoursefaculty } from './course.interface';
+import QuireBuilder from '../../builder/QueryBuilder';
+import { CourseSearchableFields } from './course.constant';
+import mongoose from 'mongoose';
+import AppError from '../../errors/AppError';
 
-const createCourseIntoDB = async (payload : TCourse) => {
-    const result = await Course.create(payload);
-    return result
-}
+const createCourseIntoDB = async (payload: TCourse) => {
+  const result = await Course.create(payload);
+  return result;
+};
 
-const getAllCourseFromDB = async (query : Record<string, unknown>) => {
+const getAllCourseFromDB = async (query: Record<string, unknown>) => {
+  const courseQuery = new QuireBuilder(
+    Course.find().populate('preRequisiteCourses.course'),
+    query,
+  )
+    .search(CourseSearchableFields)
+    .filter()
+    .paginate()
+    .sort()
+    .fields();
 
-    const courseQuery = new QuireBuilder(
-        Course.find().populate('preRequisiteCourses.course'),
-        query
-    ).search(CourseSearchableFields).filter().paginate().sort().fields()
+  const result = await courseQuery.modelQuery;
+  const meta = await courseQuery.countTotal();
+  return {
+    meta,
+    result,
+  };
+};
 
-    const result = await courseQuery.modelQuery;
-    return result
-}
+const getSingleCourseFromDB = async (id: string) => {
+  const result = await Course.findById(id).populate(
+    'preRequisiteCourses.course',
+  );
+  return result;
+};
 
-const getSingleCourseFromDB = async (id : string) => {
-    const result = await Course.findById(id).populate('preRequisiteCourses.course');
-    return result
-}
+const updateSingleCourseIntoDB = async (
+  payload: Partial<TCourse>,
+  id: string,
+) => {
+  const { preRequisiteCourses, ...courseRemainingData } = payload;
 
-const updateSingleCourseIntoDB = async (payload: Partial<TCourse>, id : string) => {
-    const {preRequisiteCourses, ...courseRemainingData} = payload;
+  const session = await mongoose.startSession();
 
-    const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
 
-    try {
-        session.startTransaction();
-
-            // step 1: update basic course data
+    // step 1: update basic course data
     const updatedBasicCourseData = Course.findByIdAndUpdate(
-        id,
-        courseRemainingData,
-        {
-            new: true,
-            runValidators: true,
-            session
-        }
-    )
+      id,
+      courseRemainingData,
+      {
+        new: true,
+        runValidators: true,
+        session,
+      },
+    );
 
     // console.log(preRequisiteCourses);
 
-
     if (!updatedBasicCourseData) {
-       throw new AppError(httpStatus.BAD_REQUEST, "failed to updating course data")
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        'failed to updating course data',
+      );
     }
-    
+
     // check if there is any preRequisiteCourses to update
     if (preRequisiteCourses && preRequisiteCourses.length > 0) {
-        // filter out the deleted fields 
-        const deletedPreRequisites = preRequisiteCourses?.filter(el => el?.course && el?.isDeleted)
-        console.log(deletedPreRequisites);
+      // filter out the deleted fields
+      const deletedPreRequisites = preRequisiteCourses?.filter(
+        (el) => el?.course && el?.isDeleted,
+      );
+      console.log(deletedPreRequisites);
 
-        const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(
-            id,
-            {
-                $pull: {  preRequisiteCourses: { course: { $in: deletedPreRequisites } } }
-            },
-            {
-                new: true,
-                runValidators: true,
-                session
-            }
-        )
-
-        if (!deletedPreRequisiteCourses) {
-            throw new AppError(httpStatus.BAD_REQUEST, "failed to updating course data")
-         }
-        
-        // filter out the new courses fields 
-    const newPrerequisites = preRequisiteCourses?.filter(el => el?.course && !el?.isDeleted);
-
-    const newPrerequisitesCourses = await Course.findByIdAndUpdate(
+      const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(
         id,
         {
-            $addToSet: { preRequisiteCourses: { $each: newPrerequisites } }
+          $pull: {
+            preRequisiteCourses: { course: { $in: deletedPreRequisites } },
+          },
         },
         {
-            new: true,
-            runValidators: true,
-            session
-        }
-    ) 
+          new: true,
+          runValidators: true,
+          session,
+        },
+      );
 
-    if (!newPrerequisitesCourses) {
-        throw new AppError(httpStatus.BAD_REQUEST, "failed to updating course data")
-     }
-} 
-        await session.commitTransaction();
-        await session.endSession();
+      if (!deletedPreRequisiteCourses) {
+        throw new AppError(
+          httpStatus.BAD_REQUEST,
+          'failed to updating course data',
+        );
+      }
 
-        const result = await Course.findById(id).populate('preRequisiteCourses.course');
-        return result;
-        
-    } catch (error) {
-        await session.abortTransaction();
-        await session.endSession();
-        throw new AppError(httpStatus.BAD_REQUEST, "failed to updating course data")
+      // filter out the new courses fields
+      const newPrerequisites = preRequisiteCourses?.filter(
+        (el) => el?.course && !el?.isDeleted,
+      );
+
+      const newPrerequisitesCourses = await Course.findByIdAndUpdate(
+        id,
+        {
+          $addToSet: { preRequisiteCourses: { $each: newPrerequisites } },
+        },
+        {
+          new: true,
+          runValidators: true,
+          session,
+        },
+      );
+
+      if (!newPrerequisitesCourses) {
+        throw new AppError(
+          httpStatus.BAD_REQUEST,
+          'failed to updating course data',
+        );
+      }
     }
-}
+    await session.commitTransaction();
+    await session.endSession();
 
-const assignFacultiesWithCourseIntoDB = async (id: string, payload: Partial<TCoursefaculty>) => {
-    const result = await CourseFaculty.findByIdAndUpdate(
-        id,
-        {
-            course: id,
-            $addToSet: {faculties: {$each: payload}}
-        },
-        {
-            upsert: true,
-            new: true,
-        }
-    )
-    return result
-}
+    const result = await Course.findById(id).populate(
+      'preRequisiteCourses.course',
+    );
+    return result;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'failed to updating course data',
+    );
+  }
+};
 
-const removeFacultiesFromCourseFromDB = async (id: string, payload: Partial<TCoursefaculty>) => {
-    const result = await CourseFaculty.findByIdAndUpdate(
-        id,
-        {
-            course: id,
-            $pull: {faculties: {$in: payload}}
-        },
-        {
-            new: true,
-        }
-    )
-    return result
-}
+const assignFacultiesWithCourseIntoDB = async (
+  id: string,
+  payload: Partial<TCoursefaculty>,
+) => {
+  const result = await CourseFaculty.findByIdAndUpdate(
+    id,
+    {
+      course: id,
+      $addToSet: { faculties: { $each: payload } },
+    },
+    {
+      upsert: true,
+      new: true,
+    },
+  );
+  return result;
+};
 
-const deleteCourseFromDB = async ( id : string ) => {
-    const result = await Course.findByIdAndUpdate(
-        id,
-        {isDeleted: true},
-        {new: true}
-    )
-    return result
-}
+const removeFacultiesFromCourseFromDB = async (
+  id: string,
+  payload: Partial<TCoursefaculty>,
+) => {
+  const result = await CourseFaculty.findByIdAndUpdate(
+    id,
+    {
+      course: id,
+      $pull: { faculties: { $in: payload } },
+    },
+    {
+      new: true,
+    },
+  );
+  return result;
+};
+
+const deleteCourseFromDB = async (id: string) => {
+  const result = await Course.findByIdAndUpdate(
+    id,
+    { isDeleted: true },
+    { new: true },
+  );
+  return result;
+};
 
 export const CourseServices = {
-    createCourseIntoDB,
-    getAllCourseFromDB,
-    getSingleCourseFromDB,
-    updateSingleCourseIntoDB,
-    assignFacultiesWithCourseIntoDB,
-    removeFacultiesFromCourseFromDB,
-    deleteCourseFromDB
-}
+  createCourseIntoDB,
+  getAllCourseFromDB,
+  getSingleCourseFromDB,
+  updateSingleCourseIntoDB,
+  assignFacultiesWithCourseIntoDB,
+  removeFacultiesFromCourseFromDB,
+  deleteCourseFromDB,
+};
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -7,10 +7,7 @@ import { TStudent } from './student.interface';
 import QuireBuilder from '../../builder/QueryBuilder';
 import { studentSearchableFields } from './student.constant';
 
-
-const getAllStudentsFromDB = async (query : Record<string, unknown>) => {
-
-
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   /*
 
   const queryObj = { ...query };
@@ -76,105 +73,115 @@ return limitQuery;
 
 */
 
+  const studentQuery = new QuireBuilder(
+    Student.find()
+      .populate('user')
+      .populate('admissionSemester')
+      .populate({
+        path: 'admissionDepartment',
+        populate: {
+          path: 'academicFaculty',
+        },
+      }),
+    query,
+  )
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
 
-const studentQuery = new QuireBuilder(Student.find()
-.populate('user')
-.populate('admissionSemester')
-.populate({
-  path: 'admissionDepartment',
-  populate: {
-  path: 'academicFaculty',
-  }
-}), 
-query)
-.search(studentSearchableFields)
-.filter().sort().paginate().fields();
-
-const result = await studentQuery.modelQuery;
-return result;
-
+  const result = await studentQuery.modelQuery;
+  const meta = studentQuery.countTotal();
+  return { meta, result };
 };
-
 
 const getSingleStudentFromDB = async (id: string) => {
   // const result = await Student.findOne({ id });
   // const result = await Student.aggregate([{ $match: { id } }]);
-  const result = await Student.findById( id )
-  .populate('admissionSemester')
-  .populate({
-    path: 'admissionDepartment',
-    populate: {
-    path: 'academicFaculty',
-    }
+  const result = await Student.findById(id)
+    .populate('admissionSemester')
+    .populate({
+      path: 'admissionDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
+  return result;
+};
+
+const updateSingleStudentFromDB = async (
+  id: string,
+  payload: Partial<TStudent>,
+) => {
+  const { name, gurdian, localGurdian, ...remainingStudentData } = payload;
+
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingStudentData,
+  };
+
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name))
+      modifiedUpdatedData[`name.${key}`] = value;
+  }
+
+  if (gurdian && Object.keys(gurdian).length) {
+    for (const [key, value] of Object.entries(gurdian))
+      modifiedUpdatedData[`gurdian.${key}`] = value;
+  }
+
+  if (localGurdian && Object.keys(localGurdian).length) {
+    for (const [key, value] of Object.entries(localGurdian))
+      modifiedUpdatedData[`localGurdian.${key}`] = value;
+  }
+
+  const result = await Student.findByIdAndUpdate(id, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
   });
   return result;
 };
 
-
-const updateSingleStudentFromDB = async (id: string, payload: Partial<TStudent>) => {
-
-  const {name, gurdian, localGurdian, ...remainingStudentData} = payload;
-
-  const modifiedUpdatedData : Record<string, unknown> = {...remainingStudentData };
-
-  if (name && Object.keys(name).length ) {
-    for(const [key, value] of Object.entries(name))
-      modifiedUpdatedData[`name.${key}`] = value
-  }
-
-
-  if (gurdian && Object.keys(gurdian).length) {
-    for(const [key, value] of Object.entries(gurdian))
-      modifiedUpdatedData[`gurdian.${key}`] = value
-  }
-      
-  if (localGurdian && Object.keys(localGurdian).length) {
-    for(const [key, value] of Object.entries(localGurdian))
-      modifiedUpdatedData[`localGurdian.${key}`] = value
-  }
-
-  const result = await Student.findByIdAndUpdate( id , modifiedUpdatedData, {new: true, runValidators: true})
-  return result;
-};
-
-
 const deleteSingleStudentFromDB = async (id: string) => {
+  const session = await mongoose.startSession();
 
-const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
 
+    const deletedStudent = await Student.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true, session },
+    );
 
-try {
-  
-  session.startTransaction()
+    if (!deletedStudent) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student');
+    }
 
-  const deletedStudent = await Student.findByIdAndUpdate( id , { isDeleted: true }, {new: true, session});
+    const userId = deletedStudent.user;
 
-  if (!deletedStudent) {
-    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student');
+    // deleted user
+    const deletedUser = await User.findByIdAndUpdate(
+      userId,
+      { isDeleted: true },
+      { new: true, session },
+    );
+    if (!deletedUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return deletedStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error('Failed to delete student');
   }
-
-  const userId = deletedStudent.user
-
-  // deleted user 
-  const deletedUser = await User.findByIdAndUpdate(userId, {isDeleted: true}, {new: true, session});
-if (!deletedUser) {
-  throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user');
-}
-
-await session.commitTransaction();
-await session.endSession()
-
-return deletedStudent;
-
-} catch (error) {
-  await session.abortTransaction();
-  await session.endSession()
-  throw new Error('Failed to delete student');
-}
 };
 
 export const StudentServices = {
-
   getAllStudentsFromDB,
   getSingleStudentFromDB,
   updateSingleStudentFromDB,
