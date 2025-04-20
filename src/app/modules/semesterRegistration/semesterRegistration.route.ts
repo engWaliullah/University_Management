9d628fdb4817ce2bf29ba9c682769e3a155e3b2b
MD Waliullah <waliullah9099@gmail.com>
2025-04-20 20:50:31 +0600

    fix course and semester-registraion issue

--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { semesterRegistrationValidation } from './semesterRegistration.validation';
 import { SemesterRegistrationController } from './semesterRegistration.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/:create-semester-registration',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     semesterRegistrationValidation.createSemesterRegistrationVadationSchema,
   ),
@@ -17,11 +20,18 @@ router.get('', SemesterRegistrationController.getAllSemesterRegistration);
 
 router.get(
   '/:id',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   SemesterRegistrationController.getSingleSemesterRegistration,
 );
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     semesterRegistrationValidation.updateSemesterRegistrationVadationSchema,
   ),
